62cd4520bc84eaaa020bf2c2c03bc88164fa7f7a
Diogo Pires <desps@iscte.pt>
2017-11-24 05:42:31 +0000

    Guis Added

---
+++ src/Gui/MainGui.java
@@ -0,0 +1,70 @@
+package Gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainGui {
+
+	private JFrame frame;
+
+	public MainGui() {
+		initialize();
+		frame.setVisible(true);
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Load Files:");
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		
+		JButton btnNewButton = new JButton("Ham");
+		panel_1.add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel_1.add(lblNewLabel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
+		
+		JButton btnNewButton_1 = new JButton("Spam");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel_2.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel_2.add(lblNewLabel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3);
+		
+		JButton btnNewButton_2 = new JButton("Ham");
+		panel_3.add(btnNewButton_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		panel_3.add(lblNewLabel_3);
+		
+		JButton btnRunTests = new JButton("Run Tests");
+		panel.add(btnRunTests);
+	}
+
+}
---
+++ src/Gui/RunGui.java
@@ -0,0 +1,72 @@
+package Gui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+
+public class RunGui {
+
+	private JFrame frame;
+	private JTable table;
+
+	public RunGui() {
+		initialize();
+		frame.setVisible(true);
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JSplitPane splitPane = new JSplitPane();
+		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		splitPane.setLeftComponent(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JButton btnNewButton_1 = new JButton("Run");
+		panel.add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("FP: ");
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("FN: ");
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton_3 = new JButton("Show Spam File");
+		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton_2 = new JButton("Show Ham File");
+		panel.add(btnNewButton_2);
+		
+		JPanel panel_1 = new JPanel();
+		splitPane.setRightComponent(panel_1);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		table = new JTable();
+		panel_1.add(table, BorderLayout.CENTER);
+		
+		JButton btnNewButton = new JButton("Configure Rules");
+		panel_1.add(btnNewButton, BorderLayout.SOUTH);
+		
+		JLabel lblSearch = new JLabel("Search");
+		panel_1.add(lblSearch, BorderLayout.NORTH);
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			//	table.set
+			}
+		});
+	}
+
+}
